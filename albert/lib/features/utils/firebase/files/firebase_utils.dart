91ede74d0f0a03311379f41b8eff965f4de80b94
Igor Arthur <igorart1210@gmail.com>
2026-07-18 13:04:42 -0300

    Fix google login

--- albert/lib/features/utils/firebase/files/firebase_utils.dart
+++ albert/lib/features/utils/firebase/files/firebase_utils.dart
@@ -1,4 +1,4 @@
-import 'package:albert/features/utils/firebase/files/firebase_options.dart';
+import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_sign_in/google_sign_in.dart';
